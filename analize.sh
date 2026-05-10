a29213d1f45ee08ece4e9c54ddae302f740ea7e6
Paul <145344090+DrPAHAN@users.noreply.github.com>
2026-05-10 17:18:36 +0700

    Add files via upload

---
+++ analize.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "=== IPv4 Header ==="
+(
+  printf "Version\tLength\tTTL\tProtocol\tChecksum\tSource Address\n"
+  tshark -r lab9_ipv4.pcap -Y icmp -T fields -e ip.version -e ip.hdr_len -e ip.ttl -e ip.proto -e ip.checksum -e ip.src -c 1 2>/dev/null
+) | column -t -s $'\t' | sed 's/\t/ | /g'
+
+echo -e "\n=== IPv6 Header ==="
+(
+  printf "Version\tPayload Len\tHop Limit\tNext Header\tSource Address\n"
+  tshark -r lab9_ipv6.pcap -Y icmpv6 -T fields -e ipv6.version -e ipv6.plen -e ipv6.hlim -e ipv6.nxt -e ipv6.src -c 1 2>/dev/null
+) | column -t -s $'\t' | sed 's/\t/ | /g'   
